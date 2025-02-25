e7378fc9f046462dad708c8736c4c6c4dfdc236a
sebastian-martinez-py <smartinezabar@gmail.com>
2025-02-24 21:14:30 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    age: int
+
+users = []  # Lista para almacenar los usuarios temporalmente
+
+@app.post("/users/")
+def create_user(user: User):
+    users.append(user)
+    return {"message": "Usuario guardado correctamente", "user": user}
+
+@app.get("/users/")
+def get_users():
+    return {"users": users}
